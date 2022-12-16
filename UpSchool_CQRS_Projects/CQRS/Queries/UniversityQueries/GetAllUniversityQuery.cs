d680b9871a6626cd76a0aafa45949643ad86dddf
ozguremine <ozguremine01@gmail.com>
2022-12-16 22:38:43 +0300

    Add MediaR package Add University modules 16/12/2022

---
+++ UpSchool_CQRS_Projects/CQRS/Queries/UniversityQueries/GetAllUniversityQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using UpSchool_CQRS_Projects.CQRS.Results.StudentResults;
+
+namespace UpSchool_CQRS_Projects.CQRS.Queries.UniversityQueries
+{
+    public class GetAllUniversityQuery : IRequest<List<GetAllStudentQueryResult>>
+    {
+
+    }
+}
